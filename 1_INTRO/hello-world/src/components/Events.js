a950b66c09022e596fdbad3cd6e3f21a366202ff
andre-mf <andremendesfelix@gmail.com>
2022-10-05 11:57:45 -0300

    Função de renderização

--- 1_INTRO/hello-world/src/components/Events.js
+++ 1_INTRO/hello-world/src/components/Events.js
@@ -1,9 +1,17 @@
 const Events = () => {
   const handleMyEvent = (e) => {
-    console.log(e)
+    console.log(e);
 
-    console.log("Ativou o evento!")
-  }
+    console.log("Ativou o evento!");
+  };
+
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso!</h1>;
+    } else {
+      return <h1>Também posso renderizar isso!</h1>;
+    }
+  };
 
   return (
     <div>
@@ -22,8 +30,10 @@ const Events = () => {
           Clica aqui, por favor.
         </button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   )
 }
 
-export default Events
+export default Events;
