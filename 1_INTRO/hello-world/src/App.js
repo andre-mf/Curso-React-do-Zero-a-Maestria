d2bad97f0b924d234ed82b627a55a1a816ad2f71
andre-mf <andremendesfelix@gmail.com>
2022-10-05 11:21:24 -0300

    Template expressions

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -3,12 +3,14 @@ import FirstComponent from "./components/FirstComponent";
 
 // styles / CSS
 import './App.css';
+import TemplateExpressions from "./components/TemplateExpressions";
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <FirstComponent/>
+      <TemplateExpressions/>
     </div>
   );
 }
