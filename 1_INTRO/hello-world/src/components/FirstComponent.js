9e3e0d97e0bcdced97bd40833a8bf45d794c228f
andre-mf <andremendesfelix@gmail.com>
2022-10-05 11:13:48 -0300

    Comentários nos componentes

--- 1_INTRO/hello-world/src/components/FirstComponent.js
+++ 1_INTRO/hello-world/src/components/FirstComponent.js
@@ -1,6 +1,15 @@
+// arquivo de estilo
+
 const FirstComponent = () => {
+  // essa função faz isso
+
+  /*
+    multi line
+   */
+
   return (
     <div>
+    {/* Algum comentário */}
       <h1>Meu primeiro componentne</h1>
     </div>
   )
