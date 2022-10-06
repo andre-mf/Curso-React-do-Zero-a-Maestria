1b3a89665c36e1fbc46eb74a36a2ba36b8777149
andre-mf <andremendesfelix@gmail.com>
2022-10-05 23:11:29 -0300

    Renderização de lista em componente

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -11,6 +11,12 @@ function App() {
   const name = "Joaquim";
   const [userName] = useState("Maria");
 
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "KIA", color: "Branco", newCar: false, km: 200000 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 32000 },
+  ];
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -31,6 +37,15 @@ function App() {
       <CarDetails brand="VW" km={100000} color="Azul" newCar={false}/>
       <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true}/>
       <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false}/>
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
