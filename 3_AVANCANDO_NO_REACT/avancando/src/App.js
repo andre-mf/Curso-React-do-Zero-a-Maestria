53ec13925b0e1e55e4c20b7b549f72d8f7992148
andre-mf <andremendesfelix@gmail.com>
2022-10-05 21:50:29 -0300

    Introdução às props

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -3,8 +3,13 @@ import City from "./assets/city.jpg";
 import ManageData from "./components/ManageData";
 import ListRender from "./components/ListRender";
 import ConditionalRender from "./components/ConditionalRender";
+import ShowUserName from "./components/ShowUserName";
+import {useState} from "react";
 
 function App() {
+  const name = "Joaquim";
+  const [userName] = useState("Maria");
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -19,6 +24,7 @@ function App() {
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+      <ShowUserName name={userName}/>
     </div>
   );
 }
