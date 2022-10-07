05d60ec20f263e6e9c7c6dc5dc2e9811a68742f3
andre-mf <andremendesfelix@gmail.com>
2022-10-06 21:01:59 -0300

    CSS de componentes

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com css</h1>
+      {/* CSS de componente */}
+      <MyComponent/>
+      <p>Este parágrafo é do App.js</p>
     </div>
   );
 }
