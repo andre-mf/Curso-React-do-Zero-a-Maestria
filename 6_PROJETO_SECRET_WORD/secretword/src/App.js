8f079eba43ae48f28e459ad13fe1b4e519f10edb
andre-mf <andremendesfelix@gmail.com>
2022-10-08 18:26:06 -0300

    Processando letras válidas e inválidas

--- 6_PROJETO_SECRET_WORD/secretword/src/App.js
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.js
@@ -70,7 +70,30 @@ function App() {
 
   // process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
+    const normalizedLetter = letter.toLowerCase();
+
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    // push guessed letter or remove a chance
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
   };
 
   // restarts the game
