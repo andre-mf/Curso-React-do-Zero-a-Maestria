bbae5501238d7f989daa0e5c867e628aff3706d1
andre-mf <andremendesfelix@gmail.com>
2022-10-08 16:32:14 -0300

    Evento de inserção de letra

--- 6_PROJETO_SECRET_WORD/secretword/src/components/Game.js
+++ 6_PROJETO_SECRET_WORD/secretword/src/components/Game.js
@@ -1,4 +1,5 @@
 import './Game.css';
+import {useRef, useState} from "react";
 
 const Game = ({
                 verifyLetter,
@@ -10,6 +11,20 @@ const Game = ({
                 guesses,
                 score
               }) => {
+
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus();
+  }
+
   return (
     <div>
       <p className="points">
@@ -32,8 +47,16 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required/>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
