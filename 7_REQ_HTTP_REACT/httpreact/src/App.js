ade23e5fccb344b677c07d249a75ee93311f19f4
andre-mf <andremendesfelix@gmail.com>
2022-10-13 23:32:40 -0300

    Custom hook para resgate de dados

--- 7_REQ_HTTP_REACT/httpreact/src/App.js
+++ 7_REQ_HTTP_REACT/httpreact/src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
 import {useEffect, useState} from "react";
 
+// 4 - custom hook
+import {useFetch} from "./hooks/useFetch";
+
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  // 4 - custom hook
+  const {data: items} = useFetch(url);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
-  useEffect(async () => {
-    async function fetchData() {
-      const res = await fetch(url);
-
-      const data = await res.json();
-
-      setProducts(data);
-    }
-
-    fetchData();
-  }, []);
+  // useEffect(async () => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //
+  //     const data = await res.json();
+  //
+  //     setProducts(data);
+  //   }
+  //
+  //   fetchData();
+  // }, []);
 
   // 2 - add de produtos
   const handleSubmit = async (e) => {
@@ -52,7 +58,7 @@ function App() {
     <div className="App">
       <h1>Lista de produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
